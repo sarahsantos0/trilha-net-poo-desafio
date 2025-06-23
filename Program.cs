e2a6d6d156b2b55656daa252747bc759512ea96a
Sarah <sarahsantosxx@gmail.com>
2025-06-23 20:49:16 -0300

    Desafio POO concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+var nokia = new Nokia("11999999999", "Nokia X20", "123456789", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("\nSmartphone iPhone:");
+var iphone = new Iphone("11988888888", "iPhone 13", "987654321", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
